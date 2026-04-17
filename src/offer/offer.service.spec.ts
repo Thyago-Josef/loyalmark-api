cff2626a4393719730314b187e2e4b7ccbf4ce01
Thyago-josef <t.y_josefcharm@hotmail.com>
2026-04-16 22:52:44 -0300

    test: fix dependency injection in offer service spec

--- src/offer/offer.service.spec.ts
+++ src/offer/offer.service.spec.ts
@@ -1,12 +1,46 @@
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { OfferService } from './offer.service';
+
+// describe('OfferService', () => {
+//   let service: OfferService;
+
+//   beforeEach(async () => {
+//     const module: TestingModule = await Test.createTestingModule({
+//       providers: [OfferService],
+//     }).compile();
+
+//     service = module.get<OfferService>(OfferService);
+//   });
+
+//   it('should be defined', () => {
+//     expect(service).toBeDefined();
+//   });
+// });
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { OfferService } from './offer.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Offer } from './entities/offer.entity';
 
 describe('OfferService', () => {
   let service: OfferService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OfferService],
+      providers: [
+        OfferService,
+        {
+          // 🛡️ Precisamos dizer ao Nest o que usar no lugar do banco de dados real
+          provide: getRepositoryToken(Offer),
+          useValue: {
+            save: jest.fn(),
+            find: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            // adicione outros métodos que você usa no service
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<OfferService>(OfferService);
